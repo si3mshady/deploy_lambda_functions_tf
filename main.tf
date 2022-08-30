f78c21399835bc047f95a3940b0ba304eb44626b
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-08-30 09:15:30 -0500

    deploying lambda function

--- main.tf
+++ main.tf
@@ -25,39 +25,56 @@ terraform {
 }
 
 
-
-
-data "aws_iam_policy_document" "lambda_assume_role_policy" {
-
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "ec2:*"
-    ]
-
-    resources = ["*"]
-
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = ["lambda:InvokeFunction"]
-
-    resources = ["*"]
-  }
-
+resource "aws_iam_role" "lambda_role" {
+name   = "lambda_role_for_creating_ami_images"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
 }
+EOF
+}
+
+
+
+resource "aws_iam_policy" "lambda_iam_policy" {
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+
+       "ec2:*"
+    
+     ],
+     "Resource": "*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        =  aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.lambda_iam_policy.arn
+}
+
+
 
 
 data "archive_file" "zip_lambda_handler" {  
@@ -67,17 +84,15 @@ data "archive_file" "zip_lambda_handler" {
 }
 
 
-resource "aws_iam_role" "lambda-iam-role" {  
-  name = "lambda-iam-role"  
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
-}
+
+
 
 
 resource "aws_lambda_function" "lambda_handler" {
         function_name = "create_wp_ami_backup"
         filename      = "handler.zip"
         source_code_hash = data.archive_file.zip_lambda_handler.output_base64sha256
-        role          = aws_iam_role.lambda-iam-role.arn
+        role          = aws_iam_role.lambda_role.arn
         runtime       = "python3.9"
         handler       = "lambda_handler.lambda_handler"
         timeout       = 10
