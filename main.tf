363f7860796d2f89fe1139ee2066031c4a49093e
Elliott Arnold <elliottarnold@Elliotts-Air.attlocal.net>
2022-08-30 08:49:35 -0500

    deploying lambda function

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_lambda_function" "lambda_handler" {
         filename      = "handler.zip"
         source_code_hash = data.archive_file.zip_lambda_handler.output_base64sha256
         role          = aws_iam_role.lambda-iam-role.arn
-        runtime       = "python3.6"
+        runtime       = "python3.9"
         handler       = "lambda_function.lambda_handler"
         timeout       = 10
 }
